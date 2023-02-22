f0184ed803cee7eafba5c4ddbdddd9101f7662ac
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2023-02-22 19:15:09 +0700

    Add files via upload

---
+++ 4_D_71220917.py
@@ -0,0 +1,18 @@
+print("Masukkan nama lengkap anda :")
+print("Masukkan prodi anda :")
+print("masukkan nilai (dalam huruf) yang anda dapat :",bumi,)
+
+if bumi< 3.75:
+    print ("Nilai adalah 3.75, kamu keren!")
+ 
+elif bumi<= 4.0:
+    print ("nilai anda adalah 4.0, hhhhhh")
+
+elif bumi <= 1:
+    print ("nilai anda adalah 1, apakah sudah ada pkiran pindah ")
+    
+
+
+
+
+    
